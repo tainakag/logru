6e36b9bd8da733ad02fd3177a6484060511e4656
tainakag <t.nkawa.83@gmail.com>
2020-04-18 22:10:54 +0900

    add sass-loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,7 @@ export default {
   modules: [
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
+    '@nuxtjs/font-awesome',
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios'
   ],
